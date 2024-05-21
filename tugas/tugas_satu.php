328aa5172742fd082ccaa05802334f1fb0147161
taufiq113 <138887746+taufiq113@users.noreply.github.com>
2024-05-21 19:44:24 +0700

    Add files via upload

---
+++ tugas/tugas_satu.php
@@ -0,0 +1,8 @@
+<?php
+echo"Nama : Taufiq Nurrohman <br> 
+Asal Kampus : Institut Informatika dan Bisnis Darmajaya <br>
+Jurusan : Teknik Informatika <br>
+Kota : Pesawaran <br>
+PHP : Pernah <br>
+HTML : Pernah <br>";
+?>
